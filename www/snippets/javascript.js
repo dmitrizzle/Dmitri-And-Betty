4015b524bceeb9824afcf50b4680bac83eb1df06
Dmitri <d@eurekaking.com>
2016-03-03 14:49:43 +0700

    Responsive adoptation
    Header menu layout for responsive
    Header menu animation to free up space
    Animation for polaroids to fill all space
    Polaroids border size to work with vertical orientated screens

--- www/snippets/javascript.js
+++ www/snippets/javascript.js
@@ -31,8 +31,9 @@ photoThrow.z.shuffle();
 // fill the rest of the arrays:
 for (var i = 0; i < totalPolaroids; ++i){
 	photoThrow.rotate[i] = Math.floor((Math.random() * 50) - 25);	
-	photoThrow.left[i] = (Math.random() * (window.getSize().x) - window.getSize().x/totalPolaroids );
-	photoThrow.top[i] = (Math.random() * (200/totalPolaroids));
+	photoThrow.left[i] = Math.random() * (window.getSize().x/totalPolaroids)*(i+1);
+	
+	photoThrow.top[i] = Math.random() * (window.getSize().y/totalPolaroids)*(i+i) + $$('body > header')[0].getSize().y/2;
 }
 
 // number increment animation:
@@ -120,7 +121,7 @@ function incrementAnimate(el){
 
 // LINKS AND NAVIGATION
 
-
+// top menu action links:
 $$('body > header a').addEvent('click', function(){
 	page('show', this);
 });
@@ -128,34 +129,42 @@ $$($$('body > header > h1'),$('background')).addEvent('click', function(){
 	page('hide');
 });
 
+// add animation class for all text and headers:
 $$('body > main p').addClass('hangingOnAString');
 $$('body > main h1').addClass('hangingOnAString');
 
-
+// scroll constructor for window:
 var scrl = new Fx.Scroll($(document.body));
 
 function page(action, el){
 	scrl.start(0,0);
 	
+	// show appropriate page/content element:
 	if(action == 'show'){
+		// hide all visible article:
 		$$('body > main article').setStyles({
 			'transform': '',
 			'opacity': '',
 			'z-index': ''
 		});
 		$$('body > main article').setStyle('opacity',0);
+		// show current article:
 		$('article'+el.get('html').replace(' ','')).setStyles({
 			'opacity': 1,
 			'transform': 'translateY(0)',
 			'z-index': 2
 		});
 		
+		// prepare content canvas:
 		$('background').addClass('blur');
 		$$('body > main').setStyle('display','block');
 		(function(){ $$('body > main').addClass('show'); }).delay(150);
+		
+		// highlight menu selections:
 		$$('body > header li').removeClass('selected');
 		el.getParents('li')[0].addClass('selected');
 		
+		// 3D rotation effect for text blocks:
 		(function(){
 		$$('body > main .hangingOnAString').each(function(el){
 			tempRotate = ((Math.random() * 0.0002) - 0);
@@ -166,16 +175,22 @@ function page(action, el){
 		});
 		}).delay(100);
 	}
+	
+	// hide all page content:
 	else if(action == 'hide'){
+		// hide all visible articles:
 		$$('body > main article').setStyles({
 			'transform': '',
 			'opacity': '',
 			'z-index': ''
 		});
 
+		// restore background:
 		$('background').removeClass('blur');
 		$$('body > main').removeClass('show');
 		(function(){ $$('body > main').setStyle('display',''); }).delay(500);
+		
+		// remove menu selections:
 		$$('body > header li').removeClass('selected');
 		$$($$('body > main p'),$$('body > main p')).each(function(el,i){
 			el.setStyle('transform', 'rotateY(0deg)');
@@ -183,6 +198,28 @@ function page(action, el){
 	}
 }
 
+
+// control header height on small screens:
+var scrollYi = 0;
+window.addEvent('scroll', function(){
+	scrollY = window.getScroll().y;
+	
+	// hide part of header on scroll down:
+	if(scrollY > 100 && window.getScroll().y > scrollYi + 10){
+		$$('body > header').addClass('hide');
+		scrollYi = scrollY;
+	}
+	
+	// restore header menu on scroll up:
+	if(
+		(scrollY < 100 || window.getScroll().y < scrollYi)
+		// avoid jumping header animation once reached bottom of page:
+		&& $(document.body).scrollHeight > ($(document.body).scrollTop + window.innerHeight + 50)
+	){
+		$$('body > header').removeClass('hide');
+		scrollYi = scrollY;
+	}
+});
 
 
 
